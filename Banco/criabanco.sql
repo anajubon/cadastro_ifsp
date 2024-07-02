59fe2af056a5673aba4eaf4ccade8607a94e42e3
bonfieti <bonfietianajulia@gmail.com>
2024-07-02 10:25:03 -0300

    1

---
+++ Banco/criabanco.sql
@@ -0,0 +1,26 @@
+drop database if exists ifsp;
+
+create database if not exists ifsp;
+
+use ifsp;
+
+create table cidade
+(
+    id          int auto_increment,
+    nome        varchar(100),
+    estado      varchar(002),
+    primary key(id)    
+);
+
+create table cliente
+(
+    id          int auto_increment,
+    nome        varchar(100),
+    email       varchar(100),
+    senha       varchar(050),
+    ativo       bool,
+    id_cidade   int,
+    primary key(id),
+    constraint fk_clientecidade
+        foreign key (id_cidade) references cidade(id)  
+);
